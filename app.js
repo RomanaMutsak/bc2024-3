3b98fa6f30c2583866dff03379a5c88dc44aff46
Romana bc2024-3 <romanamutsak2005@gmail.com>
2024-09-28 21:44:59 +0300

    Finally, app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const { program } = require('commander');
+const { Command } = require('commander');
 const fs = require('fs');
+const path = require('path');
+
+const program = new Command();
 
 program
-    .requiredOption('-i, --input <path>', 'input file')
-    .option('-o, --output <path>', 'output file')
-    .option('-d, --display', 'display result in console');
-
-program.parse(process.argv);
+    .requiredOption('-i, --input <file>', 'input JSON file')
+    .option('-o, --output <file>', 'output file')
+    .option('-d, --display', 'display result in console')
+    .parse(process.argv);
 
 const options = program.opts();
 
@@ -21,4 +23,35 @@ const inputPath = path.resolve(options.input);
 if (!fs.existsSync(inputPath)) {
     console.error('Cannot find input file');
     process.exit(1);
+}
+
+// „итанн€ JSON файлу
+let jsonData;
+try {
+    jsonData = JSON.parse(fs.readFileSync(inputPath, 'utf8'));
+} catch (err) {
+    console.error('Error reading or parsing input file');
+    process.exit(1);
+}
+
+let result = "";
+for (let i = 0; i < jsonData[0].exchangedate.length; i++) {
+    result += `${ jsonData[0].exchangedate[i] }: ${ jsonData[0].rate[i] } \n`;
+}
+
+// якщо задано параметр --display, виводимо результат в консоль
+if (options.display) {
+    console.log(result);
+}
+
+// якщо задано параметр --output, записуЇмо результат у файл
+if (options.output) {
+    const outputPath = path.resolve(options.output);
+    fs.writeFileSync(outputPath, result, 'utf8');
+    console.log(`Result saved to ${outputPath}`);
+}
+
+// якщо не задано н≥ --output, н≥ --display, програма н≥чого не робить
+if (!options.output && !options.display) {
+    process.exit(0);
 }
